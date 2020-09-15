f490edb6eba8ce6f1272fab39988c3f7b53a860f
erlementovich <BycsorucAvyith1>
2020-09-15 18:07:46 +0300

    fix roles

--- functions.php
+++ functions.php
@@ -11,6 +11,37 @@ define('MY_PLUGIN_NAME', plugin_basename(__DIR__));
 define('MY_PLUGIN_PlACE', plugins_url(MY_PLUGIN_NAME));
 
 /* register roles */
+function add_roles_on_plugin_activation()
+{
+    $manager = get_role('editor');
+    add_filter('show_admin_bar', '__return_false');
+
+    $caps = [
+        'delete_published_pages',
+        'delete_private_pages',
+        'delete_pages',
+        'delete_others_pages',
+        'edit_pages',
+        'edit_others_pages',
+        'edit_published_pages',
+        'publish_pages',
+        'read_private_pages',
+        'edit_private_pages',
+        'unfiltered_html'
+    ];
+
+    foreach ($caps as $cap) {
+        $manager->remove_cap($cap);
+    }
+
+    add_role('content_manager', 'Контент менеджер', $manager->capabilities);
+}
+
+function remove_roles_on_plugin_activation()
+{
+    remove_role('content_manager');
+}
+
 register_activation_hook(__FILE__, 'add_roles_on_plugin_activation');
 register_deactivation_hook(__FILE__, 'remove_roles_on_plugin_activation');
 
